fd8361ab0a0ce015982a7b1c796db387a9a9d5b1
Faruk <109393466+farukbeygo@users.noreply.github.com>
2022-12-04 01:14:33 +0300

    Update area.cpp

--- Main_Code/area.cpp
+++ Main_Code/area.cpp
@@ -3,10 +3,10 @@
 int main() {
     // Write C++ code here
     std::cout << "Your Bilkenter starts the its journey!!";
-    // I am creating a 10x10 area for our bilkenter
-    for (int i = 0; i<10; i++){
+    // I am creating a 50x50 area for our bilkenter
+    for (int i = 0; i<50; i++){
         std::cout << "\n";
-        for (int j = 0; j<10; j++){
+        for (int j = 0; j<50; j++){
             std::cout << "*";
         }
     }
